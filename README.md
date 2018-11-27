630bd46a2040e805a7aa562822da533a572ee143
neoctobers <neoctobers@gmail.com>
2018-11-27 12:18:35 +0800

    get_eth_price/get_eth_supply/get_eth_balance/get_eth_balances

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# etherscan
+# Etherscan.io API wrapper
 
 Etherscan.io API wrapper, for Python
+
+
+## Installation
+```
+pip3 install etherscan
+```
+
+## Usage
+```python
+import etherscan
+
+es = etherscan.Client(
+    api_key='YOUR_API_KEY',
+)
+
+eth_price = es.get_eth_price()
+
+eth_supply = es.get_eth_supply()
+
+eth_balance = es.get_eth_balance('0x39eB410144784010b84B076087B073889411F878')
+
+eth_balances = es.get_eth_balances([
+    '0x39eB410144784010b84B076087B073889411F878',
+    '0x39eB410144784010b84B076087B073889411F879',
+])
+```
--- etherscan/etherscan.py
+++ etherscan/etherscan.py
@@ -6,48 +6,91 @@ class Client():
 
     def __init__(self,
                  api_key: str,
-                 network='mainnet',
+                 network=None,
                  ):
-        if network not in ['mainnet', 'ropsten', 'kovan', 'rinkeby']:
-            raise Exception('network could only be mainnet/ropsten/kovan/rinkeby')
-
-        self._api_key = api_key
 
         # Base URL
-        self._base_url = 'https://api.etherscan.io/api?'
+        self._base_url = 'https://api.etherscan.io/api'
+
+        # API Key
+        self._api_key = api_key
+
+        # Network
+        if network:
+            if network not in ['ropsten', 'kovan', 'rinkeby']:
+                raise Exception('network could only be None(mainnet) /ropsten/kovan/rinkeby')
+
+            self._base_url = 'https://api-{network}.etherscan.io/api'.format(
+                network=network
+            )
 
         # Endpoint URL
-        self._endpoint = 'https://api.etherscan.io/api?'
+        self._url = self._base_url
 
-        # # headers
-        # self._headers = {
-        #     'Content-Type': 'application/json',
-        # }
-        #
-        # # params
-        # self._params = []
-        #
-        # # payload
-        # self._payload = {
-        #     'jsonrpc': '2.0',
-        #     'id': 1,
-        # }
+        # params
+        self._params = {
+            'apikey': self._api_key,
+        }
 
-    def req(self):
-        # self._payload['params'] = self._params
+    def _req(self):
+        # queries
+        queries = []
+        for key, value in self._params.items():
+            queries.append('{key}={value}'.format(
+                key=key,
+                value=value,
+            ))
 
-        r = requests.get(
-            url=self._endpoint,
-        ).json()
+        # url
+        self._url = '{base_url}?{query_string}'.format(
+            base_url=self._base_url,
+            query_string='&'.join(queries)
+        )
+
+        # get, json
+        r = requests.get(url=self._url).json()
 
         if '1' == r['status']:
             return r['result']
+        else:
+            # todo: handle with error
+            print(r)
 
-        # todo: handle with error
         return r
 
     def get_eth_price(self):
-        self._endpoint = 'https://api.etherscan.io/api?module=stats&action=ethprice&apikey={}'.format(self._api_key)
+        self._params['module'] = 'stats'
+        self._params['action'] = 'ethprice'
 
-        return self.req()
+        r = self._req()
 
+        return {
+            'ethbtc': float(r['ethbtc']),
+            'ethbtc_timestamp': int(r['ethbtc_timestamp']),
+            'ethusd': float(r['ethusd']),
+            'ethusd_timestamp': int(r['ethbtc_timestamp']),
+        }
+
+    def get_eth_supply(self):
+        self._params['module'] = 'stats'
+        self._params['action'] = 'ethsupply'
+
+        return int(self._req())
+
+    def get_eth_balance(self, address: str):
+        self._params['module'] = 'account'
+        self._params['action'] = 'balance'
+        self._params['address'] = address
+
+        return int(self._req())
+
+    def get_eth_balances(self, addresses: list):
+        self._params['module'] = 'account'
+        self._params['action'] = 'balancemulti'
+        self._params['address'] = ','.join(addresses)
+
+        balances = {}
+        for row in self._req():
+            balances[row['account']] = int(row['balance'])
+
+        return balances
