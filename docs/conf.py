4caacf95c05cd59a0339499195a72af73fa5d3cb
neoctobers <neoctobers@gmail.com>
2018-12-05 14:42:46 +0800

    add markdown

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+from recommonmark.parser import CommonMarkParser
 
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 
 # -- Project information -----------------------------------------------------
 
@@ -47,8 +51,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
